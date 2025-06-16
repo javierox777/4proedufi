962e3c8324d9c0e01929298fe84b684e01d5613c
javier allendes <javierallendes@MacBook-Pro-de-javier.local>
2025-06-16 08:44:48 -0400

    se agrego rut al modelo de alumno

--- src/models/alumnos/alumno.model.js
+++ src/models/alumnos/alumno.model.js
@@ -4,7 +4,8 @@ const {Schema, model} = require("mongoose")
 const schemaAlumno = new Schema({
     nombre:String,
     apellido:String,
-    edad:Number
+    edad:Number,
+    rut:String
 })
 
 module.exports = model("alumnos", schemaAlumno )
